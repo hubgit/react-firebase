b0efcf84c2e0a10ba1b15d058688b8bf57a649ed
Alf Eaton <eaton.alf@gmail.com>
2018-01-20 14:31:30 +0000

    Export connect from react-redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Auth } from './components/Auth'
 export { default as User } from './components/User'
 export { default as FirebaseProvider } from './components/FirebaseProvider'
+export { connect } from 'react-redux'
